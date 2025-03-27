8347df8e074dbb752706d9afc7b9696771d98be4
ketsp8ce <hunter.marx@snhu.edu>
2025-03-27 18:20:33 -0500

    deleted the test files that I had written manually to practice creating JUnit tests through the GUI in Eclipse, will fill back in

--- src/test/java/io/github/ketsp8ce/contactservice/test/ContactServiceTest.java
+++ src/test/java/io/github/ketsp8ce/contactservice/test/ContactServiceTest.java
@@ -1,50 +1,54 @@
 package io.github.ketsp8ce.contactservice.test;
 
-//Author Name: hunter Marx
-
-//Date: 03/27/2025
-
-//Course ID: CS32010813-M01
-
-//Description: This is the unit tests for the contact service (ContactServiceTest)
-
-
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
-
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.util.ArrayList;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
-import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
 
-@TestMethodOrder(OrderAnnotation.class)
-public class ContactServiceTest {
+class ContactServiceTest {
 
-/*
-* The following tests exercise the ContactService class.
-* In each test, a new service is created with default values for each field.
-* Then the service is requested to make some change to the list of contacts and the result
-* is tested to ensure the actual field matches what is expected.
-*
-* Because each contact that gets created has a new automatically assigned contactID,
-* and the tests are run based on that contactID, the order in which the tests are run depend
-* on the value of each contactID. Therefore, the @Order annotation is used to keep the tests
-* in a specific order.
-*
-* For evidence that the contactID is correct for each test, uncomment the service.displayContactList();
-* prior to each assertion so that the records are displayed on the console for error checking.
-*/
-@Test
-@DisplayName("Test to Update First Name.")
-@Order(1)
-void testUpdateFirstName() {
-ContactService service = new ContactService();
-service.addContact("Dr.", "Cross", "5555551111", "123 Lollypop Lane");
-service.updateFirstName("Sven", "0");
-//service.displayContactList();
-assertEquals("Sven",service.getContact("0").getFirstName(), "First name was not updated.");
+	@Test
+	void testDisplayContactList() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testAddContact() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testDeleteContact() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testUpdateFirstName() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testUpdateLastName() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testUpdateNumber() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testUpdateAddress() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testGetContact() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testGetContactList() {
+		fail("Not yet implemented"); // TODO
+	}
+
 }
-
-// Now it's your turn to finish creating the code 
--- src/test/java/io/github/ketsp8ce/contactservice/test/ContactTest.java
+++ src/test/java/io/github/ketsp8ce/contactservice/test/ContactTest.java
@@ -1,78 +1,59 @@
-//Author Name: Hunter Marx
-
-//Date: 03/27/2025
-
-//Course ID: CS320-10813-M01
-
-//Description: This is the unit tests for the contact class (ContactTest).
-
 package io.github.ketsp8ce.contactservice.test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-import io.github.ketsp8ce.contactservice.main.Contact;
+class ContactTest {
 
-import org.junit.jupiter.api.DisplayName;
-import static org.junit.jupiter.api.Assertions.*;
+	@Test
+	void testContact() {
+		fail("Not yet implemented"); // TODO
+	}
 
-public class ContactTest {
-/*
-* The following tests exercise the Contact class.
-* The first 5 tests to make sure the field does not become longer than the constraint
-* (10 characters for first and last name, exactly 10 characters for phone number,
-* and 30 characters for the address).
-* The last 4 tests ensure that each field is not null.
-* ContactID is not tested for being not null because there isn't a way to create
-* a contact with a null contactID. Likewise it is not tested for being non-updateable
-* because there is no way to update it.
-*/
-@Test
-@DisplayName("Contact ID cannot have more than 10 characters")
-void testContactIDWithMoreThanTenCharacters() {
-Contact contact = new Contact("FirstName","LastName","PhoneNumbr","Address");
-if(contact.getContactID().length() > 10) {
-fail("Contact ID has more than 10 characters.");
-}
-}
+	@Test
+	void testGetContactID() {
+		fail("Not yet implemented"); // TODO
+	}
 
-@Test
-@DisplayName("Contact First Name cannot have more than 10 characters")
-void testContactFirstNameWithMoreThanTenCharacters() {
-Contact contact = new Contact("OllyOllyOxenFree","LastName","PhoneNumbr","Address");
-if(contact.getFirstName().length() > 10) {
-fail("First Name has more than 10 characters.");
-}
-}
+	@Test
+	void testGetFirstName() {
+		fail("Not yet implemented"); // TODO
+	}
 
-@Test
-@DisplayName("Contact Last Name cannot have more than 10 characters")
-void testContactLastNameWithMoreThanTenCharacters() {
-Contact contact = new Contact("firstName","supercalifragalisticexpialidosious","PhoneNumbr","Address");
-if(contact.getLastName().length() > 10) {
-fail("Last Name has more than 10 characters.");
-}
-}
+	@Test
+	void testGetLastName() {
+		fail("Not yet implemented"); // TODO
+	}
 
-@Test
-@DisplayName("Contact phone cannot have more than 10 characters")
-void testContactPhoneWithMoreThanTenCharacters() {
-Contact contact = new Contact("firstName","lastName","111111111111111111","Address");
-if(contact.getNumber().length() > 10) {
-fail("Number has more than 10 characters.");
-}
-}
+	@Test
+	void testGetNumber() {
+		fail("Not yet implemented"); // TODO
+	}
 
+	@Test
+	void testGetAddress() {
+		fail("Not yet implemented"); // TODO
+	}
 
-@Test
-@DisplayName("Contact adress cannot have more than 10 characters")
-void testContactAddressWithMoreThanTenCharacters() {
-Contact contact = new Contact("firstName","lastName","PhoneNumbr","6666666666 spookeydookeyookie st, HIILHIILHIILHIILHIIL, 1234567890987654321");
-if(contact.getAddress().length() > 10) {
-fail("Address has more than 10 characters.");
-}
-}
+	@Test
+	void testSetFirstName() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testSetLastName() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testSetNumber() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testSetAddress() {
+		fail("Not yet implemented"); // TODO
+	}
 
 }
-
-// Now it's your turn to finish creating the code 
-
